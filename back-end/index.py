adbe0d046e5efb7f4340fc062416d08f35333557
arsummers <aliyasummers1@gmail.com>
2019-07-15 23:22:22 -0700

    server is responsive

--- back-end/index.py
+++ back-end/index.py
@@ -17,9 +17,9 @@ class Locations():
 
     def get_data_for_location(self, url):
         result = requests.get(url).json()
-        self.formatted_query = result['results'][0]['formatted_adress']
+        self.formatted_query = result['results'][0]['formatted_address']
         self.latitude = result['results'][0]['geometry']['location']['lat']
-        self.longitude = result['results'][0]['geometry']['location']['lat']
+        self.longitude = result['results'][0]['geometry']['location']['lng']
 
 
     def serialize(self):
@@ -34,30 +34,34 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         print('request path', parsed_path)
         print('parsed query', parse_qs)
 
-    if parsed_path.path == '/locations':
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        if parsed_path.path == '/locations':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+
+
+            # used to access the geocode API key in my hidden .env file
+            GEOCODE_API_KEY = os.getenv('GEOCODE_API_KEY')
+
+            parsed_query_string = parse_qs(parsed_path.query)
+            query = parsed_query_string['data'][0]
+            url = f'https://maps.googleapis.com/maps/api/geocode/json?address={query}&key={GEOCODE_API_KEY}'
+            location = Locations(url, query)
+            print('geocode url', url)
+
+            result = requests.get(url).json()
+
+            formatted_query = result['results'][0]['formatted_address']
+
+            print('formatted query: ', formatted_query)
+
+            json_string = json.dumps(location.serialize())
+            print(json_string.encode())
+            self.wfile.write(json_string.encode())
+            return
+
+        self.send_response_only(404)
         self.end_headers()
-
-        query = 'barcelona'
-        # used to access the geocode API key in my hidden .env file
-        GEOCODE_API_KEY = os.getenv('GEOCODE_API_KEY')
-        url = f'https://maps.googleapis.com/maps/api/geocode/json?address={query}&key={GEOCODE_API_KEY}'
-        print('geocode url', url)
-
-        result = requests.get(url).json()
-
-        formatted_query = result['results'][0]['formatted_address']
-
-        print('formatted query: ', formatted_query)
-
-        json_string = json.dumps(location.serialize())
-        print(json_string.encode())
-        self.wfile.write(json_string.encode())
-        return
-
-    self.send_response_only(404)
-    self.end_headers()
 
 
 
