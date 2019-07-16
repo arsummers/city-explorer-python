3d95b1ca530bdc4126718767da0559c8f13ff3c0
arsummers <aliyasummers1@gmail.com>
2019-07-15 23:47:27 -0700

    server communicating

--- back-end/index.py
+++ back-end/index.py
@@ -4,11 +4,16 @@ import os
 import json
 import requests
 
+# sets up the port, allows it to access from the hidden .env file
 from dotenv import load_dotenv
 load_dotenv()
 
+# used to access the geocode API key in my hidden .env file. Keeping these global so they can be easily accessed below as needed.
 
+GEOCODE_API_KEY = os.getenv('GEOCODE_API_KEY')
+PORT = os.getenv('PORT')
 
+# builds from both classes in the class demo - if I incorporate more APIS, I will have more classes.
 class Locations():
     def __init__(self, url, query):
         self.url = url
@@ -17,8 +22,14 @@ class Locations():
 
     def get_data_for_location(self, url):
         result = requests.get(url).json()
+
+        # Digs into the JSON object the API gave back to me via postman
+        # Works a lot like the dot notation I used to dig into the same API when I did city explorer in javascript
+
         self.formatted_query = result['results'][0]['formatted_address']
+
         self.latitude = result['results'][0]['geometry']['location']['lat']
+
         self.longitude = result['results'][0]['geometry']['location']['lng']
 
 
@@ -31,20 +42,18 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         parsed_path = urlparse(self.path)
-        print('request path', parsed_path)
-        print('parsed query', parse_qs)
+
 
         if parsed_path.path == '/locations':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
 
-
-            # used to access the geocode API key in my hidden .env file
-            GEOCODE_API_KEY = os.getenv('GEOCODE_API_KEY')
-
             parsed_query_string = parse_qs(parsed_path.query)
+
+            # helps clean up the response
             query = parsed_query_string['data'][0]
+
             url = f'https://maps.googleapis.com/maps/api/geocode/json?address={query}&key={GEOCODE_API_KEY}'
             location = Locations(url, query)
             print('geocode url', url)
@@ -64,20 +73,18 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
 
-
-
-
 # create server environment
 def create_server():
     return HTTPServer(
-        ('127.0.0.1', 3000), SimpleHTTPRequestHandler
+        # since PORT is a variable in my .env, and it is read as a string, since Python doesn't have the same level of type coercion as javascript, I've turned it into an integer to keep the port running
+        ('127.0.0.1', int(PORT)), SimpleHTTPRequestHandler
     )
 
 def run_forever():
     server = create_server()
 
     try:
-        print('Starting server on Port 3000')
+        print(f'Starting server on Port {PORT}')
         server.serve_forever()
 
     except:
