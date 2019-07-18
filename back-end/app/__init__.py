09d615b85ae63cee48c9ce6a7439786e4b5d22e5
arsummers <aliyasummers1@gmail.com>
2019-07-17 20:45:53 -0700

    all locations from db route working

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -18,7 +18,7 @@ migrate = Migrate(app, db)
 
 @app.route('/location', methods=['GET'])
 def new_location():
-# check db:
+#TODO: check db:
     # if search name not in db, do the API stuff, insert into db
 #if name in db, return the saved values
 
@@ -29,24 +29,18 @@ def new_location():
 
 @app.route('/testdb')
 def test_loc_db():
-    print('**************************************************')
-    # location = LocationsModel.query.filter_by(name='seattle').first()
+
     newlocation = LocationsModel(name='seattle', formatted_query='Seattle, WA, USA', latitude=47.6062095, longitude=-122.3320708)
-    # formatted_city = (formatted_query='Seattle, WA, USA')
-    # latitude = LocationsModel(latitude=47.6062095)
-    # longitude = LocationsModel(longitude=-122.3320708)
-
     db.session.add(newlocation)
-    # db.session.add(formatted_city)
-    # db.session.add(latitude)
-    # db.session.add(longitude)
-
-
     db.session.commit()
 
     return jsonify(newlocation.convert_to_dict())
 
+@app.route('/api/v1/locations')
+def all_locations():
 
+    newlocation = [location.convert_to_dict() for location in LocationsModel.query.all()]
+    return jsonify(newlocation)
 
 @app.route('/weather', methods=['GET'])
 def weather():
